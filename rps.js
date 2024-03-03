0338a53dedf76672b8dca28beec34cd191c672e2
atreska <115714570+atreska@users.noreply.github.com>
2024-03-03 12:49:23 -0500

    Delete comment

--- rps.js
+++ rps.js
@@ -1,11 +1,4 @@
-/*
-Ask the user to enter in one of three choices, Rock, Paper, or Scissors.
-Compare the users choice to the computers selection.
-Rock beats Scissors, Paper Beats Rock, and Scissors beats paper.
-If the users choice beats the computers choice, the user wins. Otherwise 
-the computer wins the round.
-Display who won once the round is over. 
-*/
+
 let body = document.querySelector('body');
 
 let rockBtn = document.createElement('button');
